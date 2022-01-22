86f8c64ef1a51af5b8cfafb80cbe8669370f9500
nicex000 <martinlagas@hotmail.com>
2022-01-23 02:26:09 +0900

    Add Pressure plate and remote activation interface

---
+++ Assets/Scripts/Interfaces/RemoteActivation.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public abstract class RemoteActivation : MonoBehaviour
+{
+    public abstract void Activate();
+    public abstract void Deactivate();
+}
---
+++ Assets/Scripts/PressurePlateScript.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+public class PressurePlateScript : MonoBehaviour
+{
+    [SerializeField] private float pressedTimer;
+    [SerializeField] private RemoteActivation activationObject;
+
+    private float timer;
+    private bool remoteState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0f;
+        remoteState = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                TryRemoteAction(false);
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.root.CompareTag("Player") || other.transform.root.CompareTag("Movable"))
+        {
+            if (pressedTimer > 0f)
+            {
+                timer = pressedTimer;
+            }
+            TryRemoteAction(true);
+        }
+    }
+
+    private void TryRemoteAction(bool activate)
+    {
+        if (activate)
+        {
+            if (!remoteState)
+            {
+                activationObject.Activate();
+                remoteState = true;
+            }
+        }
+        else
+        {
+            if (remoteState)
+            {
+                activationObject.Deactivate();
+                remoteState = false;
+            }
+        }
+    }
+}
