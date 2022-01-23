32c59e54d8cabdb100ed8e0d689b17db4fb26a92
Raffaele Ariano <raffaele.ariano@protonmail.com>
2022-01-23 21:51:54 +0100

    commit test combattimento + normalizzazione movimento

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -23,9 +23,13 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         float baseSpeed = playerSpeed * Time.fixedDeltaTime;
-        Vector3 v = Vector3.right * inputX * baseSpeed +
-            Vector3.forward * inputY * baseSpeed;
-        rbody.velocity = v;
+        Vector3 v = Vector3.right * inputX +
+            Vector3.forward * inputY;
+
+        if (v.sqrMagnitude > 1)
+            v = v.normalized;
+
+        rbody.velocity = v * baseSpeed;
         if (drawDebug) Debug.DrawLine(transform.position, transform.position + v.normalized * v.magnitude/4, Color.red, 1);
     }
 }
