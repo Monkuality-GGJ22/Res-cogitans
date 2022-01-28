c694ffbe0c710ffa09f8c2923a599ac9cfd676c6
Niccolò Cardelli <34963602+sadmozer@users.noreply.github.com>
2022-01-28 21:47:07 +0100

    Level 1 tilemap test

---
+++ Assets/Maps/Level 1 - Animal/tsx/cristallo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Cristallo" tilewidth="64" tileheight="64" tilecount="14" columns="14">
+ <image source="../../../Sprites/cristallo.png" width="896" height="64"/>
+</tileset>
