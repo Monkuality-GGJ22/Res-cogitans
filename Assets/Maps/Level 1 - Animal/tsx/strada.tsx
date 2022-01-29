94934c4f2cc3a2463522729e3293605be5d48452
Niccolò Cardelli <34963602+sadmozer@users.noreply.github.com>
2022-01-29 10:03:27 +0100

    Secretiveness level

--- Assets/Maps/Level 1 - Animal/tsx/strada.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Brain" tilewidth="64" tileheight="64" tilecount="16" columns="8">
- <image source="../../../Sprites/Wall 1.png" width="512" height="128"/>
-</tileset>
